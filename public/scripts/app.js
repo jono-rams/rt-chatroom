91268c2d4147cca5e922ab07d5610a35568217ee
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-23 07:54:00 -0400

    Sending new chats

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,21 @@
 // DOM queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const msg = newChatForm.message.value.trim();
+  if (msg && msg !== '') {
+    chatRoom.addChat(msg)
+      .then(() => newChatForm.reset())
+      .catch((err) => console.error(err));
+  }
+});
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatRoom = new Chatroom('general', 'shaun');
+const chatRoom = new Chatroom('general', 'anon');
 
 // get chats and render
 chatRoom.getChats(data => chatUI.render(data));
