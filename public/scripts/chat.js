c5a8c81a187d730eb0ed24ff6124e8e74678b92f
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-23 07:28:26 -0400

    Updating the name and room

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -4,12 +4,13 @@
 // updating the room
 
 class Chatroom {
-  constructor(room, username){
+  constructor(room, username) {
     this.room = room;
     this.username = username;
     this.chats = db.collection('chats');
+    this.unsub;
   }
-  async addChat(message){
+  async addChat(message) {
     // format a chat object
     const now = new Date();
     const chat = {
@@ -22,18 +23,28 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
-  getChats(callback){
-    this.chats
+  getChats(callback) {
+    this.unsub = this.chats
       .where('room', '==', this.room)
       .orderBy('created_at', 'asc')
       .onSnapshot(snapshot => {
         snapshot.docChanges().forEach(change => {
-          if(change.type === 'added'){
+          if (change.type === 'added') {
             // update the ui
             callback(change.doc.data());
           }
         })
       });
+  }
+  updateName(username) {
+    this.username = username;
+    localStorage.setItem('username', username);
+  }
+  updateRoom(room) {
+    this.room = room;
+    if (this.unsub) {
+      this.unsub();
+    }
   }
 }
 
